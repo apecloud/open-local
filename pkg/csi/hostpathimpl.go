e2275d91a0585453c32a1113076c9d55c8020984
gufeijun <gugoings@gmail.com>
2023-11-30 18:16:24 +0800

    fix

--- pkg/csi/hostpathimpl.go
+++ pkg/csi/hostpathimpl.go
@@ -108,7 +108,7 @@ FS=$(stat -f -c %T "{{ .BasePath }}")
 if [[ "$FS" == "xfs" ]]; then
   PID=$(cat {{ .ProjectIDFile }})
   xfs_quota -x -c "limit -p bsoft=0 bhard=0 $PID" "{{ .BasePath }}"
-  rm {{ .ProjectIDFile }}
+  rm {{ .ProjectIDFile }} || true
 fi
 rm -rf {{ .ProjectPath }} || true
 	`
